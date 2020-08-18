e97ffda590b95102f81ce1b0cb6ee6ce9140d1d0
Javier <jsm88_live@hotmail.com>
2020-08-18 11:19:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ require('firebase-admin')
 // See https://firebase.google.com/docs/web/setup for how to
 // auto-generate this config
 var firebaseConfig = {
-    apiKey: "AIzaSyC6KTeMWTLKQaEVanjc2ld9OV7ZNA2YZeU",
+    apiKey: "",
     authDomain: "cunino-1eecc.firebaseapp.com",
     databaseURL: "https://cunino-1eecc.firebaseio.com",
     projectId: "cunino-1eecc",
     storageBucket: "cunino-1eecc.appspot.com",
     messagingSenderId: "930373808585",
-    appId: "1:930373808585:web:5a82806c40c8702c8f3818",
+    appId: "",
     measurementId: "G-BSKHPEP70B"
   };
   
